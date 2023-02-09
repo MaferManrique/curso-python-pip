8c583cc8280d600b2466ba6f719cb3166d6939df
Mafer Manrique <mafer2507@gmail.com>
2023-02-09 17:36:50 +0100

    pandas

---
+++ web-server/store.py
@@ -0,0 +1,9 @@
+import requests 
+
+def get_categories():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    categories = r.json()
+    for category in categories:
+        print(category['name'])
